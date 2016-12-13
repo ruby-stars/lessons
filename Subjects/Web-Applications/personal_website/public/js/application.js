d9ae7d69f40bbd7d2812440813ae0513e1e6ed00
John Mario <johnmario91@gmail.com>
2016-12-13 22:16:17 +0100

    Javascript and jQuery

--- Subjects/Web-Applications/personal_website/public/js/application.js
+++ Subjects/Web-Applications/personal_website/public/js/application.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+  // $("section").fadeOut(3000).fadeIn(3000);
 
+  $("#header a").click(function(){
+    // Here we want to dissapear AND reappear the section to which I go
+    var section = $(this).attr("href"); // This will be #projects or #about-me
+    $(section).fadeOut(3000).fadeIn(3000);
+  });
+
+  $("#toggle-links").click(function(){
+    $(".links-section").toggle();
+  });
 });
